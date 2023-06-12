f68a97b31ce54de97768942177b868ccb3a808b9
mhanna112-code <Marc.Hanna-1@ou.edu>
2023-06-12 12:28:00 -0500

    made new function

--- main.py
+++ main.py
@@ -16,6 +16,10 @@ def print_anthony():
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi im anthony')  # Press ⌘F8 to toggle the breakpoint.
 
+def print():
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi im m')  # Press ⌘F8 to toggle the breakpoint.
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
