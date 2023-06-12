5f97dbaee57988c170cba6b4c9083fa96265e697
Marc Hanna <Marc.Hanna-1@ou.edu>
2023-06-12 12:21:09 -0500

    made anthony function

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ def print_marc():
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi im marc')  # Press ⌘F8 to toggle the breakpoint.
 
+def print_anthony():
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi im anthony')  # Press ⌘F8 to toggle the breakpoint.
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
