5be86b867dd85abd7509e22cd51b629bf6a13259
mhanna112-code <Marc.Hanna-1@ou.edu>
2023-07-08 21:32:55 -0500

    nice comments!

--- main.py
+++ main.py
@@ -25,3 +25,7 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+#qjnefjqnekfjnjenfj
+#rjqkejqfnjke2
+#rjkq3njnqvkj3rnvkjrnjrnkf
